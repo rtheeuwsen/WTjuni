035cdfe74c39cf7d872bdd387bc87acb01aac1b4
rtheeuwsen <47597771+rtheeuwsen@users.noreply.github.com>
2020-06-17 17:34:06 +0200

    doublefor

---
+++ vieropeenrij/vieropeenrij.js
@@ -0,0 +1,17 @@
+function start(vakId){  
+  
+    for(var x = 0 ; x < 2 ; x++){
+        for(var y = 0 ; y < 2 ; y++) {
+        
+            var vakId = "vak"+x+y;
+            console.log(vakId)  
+        }
+    }
+}
+
+
+           
+
+var elem = document.getElementById(vakId);
+            elem.bgColor = 'red';
+    console.log(vakId);
