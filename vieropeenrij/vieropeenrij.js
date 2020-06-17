00f950a4b44c6d00ac1d2e62714b54e2b7285095
rtheeuwsen <47597771+rtheeuwsen@users.noreply.github.com>
2020-06-17 17:57:12 +0200

    3x3yloop

--- vieropeenrij/vieropeenrij.js
+++ vieropeenrij/vieropeenrij.js
@@ -1,13 +1,29 @@
 function start(vakId){  
-  
-    for(var x = 0 ; x < 2 ; x++){
-        for(var y = 0 ; y < 2 ; y++) {
+
+    var elem = document.getElementById(vakId);
+            elem.bgColor = 'red';
+    console.log(elem.bgColor);
+    console.log(vakId[3]);
+    var x=vakId[3];
+
+
+        for(var y = 2 ; y < 0 ; y--) {
         
-            var vakId = "vak"+x+y;
-            console.log(vakId)  
+            var vakId = "vak"+x+y; 
+            console.log(vakId)
+            if (elem.bgColor == "red") {
+                console.log(46)
+            }
+
+
+
+
         }
+        
     }
-}
+
+
+
 
 
            
