13b6a584d961f11f35ee398bd3271393abc8fb86
rtheeuwsen <47597771+rtheeuwsen@users.noreply.github.com>
2020-06-17 12:22:46 +0200

    3x3klik+hover

---
+++ vieropeenrij.js
@@ -0,0 +1,13 @@
+function start(vakId){
+
+    for(var x = 0 ; x < 2 ; x++){
+
+            var vakjeId = "vak"+x;
+            console.log(vakId);
+
+            var elem = document.getElementById(vakId);
+            elem.bgColor = 'red';
+            
+    }
+
+}
