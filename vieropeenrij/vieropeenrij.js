86ad1ddea39584bba6fd3f0b4c2246298b3734cd
rtheeuwsen <47597771+rtheeuwsen@users.noreply.github.com>
2020-06-18 10:32:08 +0200

    workingdrop

--- vieropeenrij/vieropeenrij.js
+++ vieropeenrij/vieropeenrij.js
@@ -1,18 +1,23 @@
 function start(vakId){  
 
-    var elem = document.getElementById(vakId);
-            elem.bgColor = 'red';
-    console.log(elem.bgColor);
-    console.log(vakId[3]);
-    var x=vakId[3];
+    
+    
+    console.log(vakId[4]);
+    var x=vakId[4];
 
 
-        for(var y = 2 ; y < 0 ; y--) {
+        for(var y = 2 ; y > -1 ; y--) {
         
-            var vakId = "vak"+x+y; 
+            var vakId = "vak"+y+x; 
+            var elem = document.getElementById(vakId);
+            
             console.log(vakId)
-            if (elem.bgColor == "red") {
+            if (elem.bgColor !== "red") {
+                elem.bgColor = 'red';
+                break;
+
                 console.log(46)
+                console.log(elem.bgColor);
             }
 
 
