271c55e62a2dbe55e5edd281f64d395814e3b6d4
rtheeuwsen <47597771+rtheeuwsen@users.noreply.github.com>
2020-06-20 15:46:12 +0200

    names and colors

--- vieropeenrij/vieropeenrij.js
+++ vieropeenrij/vieropeenrij.js
@@ -1,34 +1,64 @@
 
-var player = true;
+var speler1 = "speler1";
+var speler2 = "speler2";
+var steenKleur1;
+var steenKleur2;
+var player;
 
+function spelerSubmit(){
+    speler1 = document.getElementById("speler1").value;
+    speler2 = document.getElementById("speler2").value;
+    player = speler1;
+    document.getElementById("spelerBeurt").innerHTML = player;
 
-function vakKleuren(vakId){  
+    kleur1 = document.getElementById("kleur1");
+    steenKleur1 = kleur1.options[kleur1.selectedIndex].value;
+
+    kleur2 = document.getElementById("kleur2");
+    steenKleur2 = kleur2.options[kleur2.selectedIndex].value;
+
+}
+
+function vakKleuren(vakId){ 
     
-    
-    console.log(vakId[4]);
-    var x=vakId[4];
-
+    console.log(steenKleur1);
+    console.log(steenKleur2); 
+      
+    var x=vakId[4];        
 
         for(var y = 5 ; y > -1 ; y--) {
         
             var vakId = "vak"+y+x; 
             var elem = document.getElementById(vakId);
+
+            if ((elem.bgColor == steenKleur1 || elem.bgColor == steenKleur2) && y == 0) {
+                alert("Kolom vol! Selecteer een andere kolom."); 
+            }
             
-            console.log(vakId)
-            if (elem.bgColor !== "red" && elem.bgColor !== "yellow" ) {
-                if (player == true) {
-                   elem.bgColor = 'red'; 
-                   player = false;
+            if (elem.bgColor !== steenKleur1 && elem.bgColor !== steenKleur2) {
+                if (player == speler1) {
+                   elem.bgColor = steenKleur1; 
+                   player = speler2;
                 }
                 else {
-                    elem.bgColor = 'yellow';
-                    player = true;
-                }
-                
+                    elem.bgColor = steenKleur2;
+                    player = speler1;
+                }   
+
+                document.getElementById("spelerBeurt").innerHTML = player;
+
                 break;
 
             }
-
+           
         }
         
     }
+
+    function reset(){
+        location.reload();
+    }
+    
+    
+    
+    /*  */
